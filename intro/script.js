223d37dcce7aedd5c33fcc4ae4f9d5518acdeece
Gabriel Yoder <GabrielYoder8@gmail.com>
2021-11-16 13:12:55 -0500

    first commit

---
+++ intro/script.js
@@ -0,0 +1,8 @@
+//alert("This is a stickup get down on the ground")
+// var x = 5;
+// var y = 37;
+
+// var answer = x + y;
+// console.log(answer);
+var fname = prompt("please enter your name:")
+alert("hello " + fname +" how are you today?")
